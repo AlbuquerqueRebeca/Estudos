4dd6c3e9f27830449607d2c0a850a00a0e4eb23a
Rebeca <becalbuquerque_23@hotmail.com>
2023-10-10 11:38:53 -0300

    Desafio 2 de filter criando produto

---
+++ ProdutoFilter/Produto.java
@@ -0,0 +1,46 @@
+package ProdutoFilter;
+
+public class Produto { 
+
+    final String nome;
+	final double produto;
+	final double desconto;
+	final double valorFrete;
+	
+	
+	
+	
+	
+	
+	
+	
+	public Produto(String nome, double produto, double desconto, double valorFrete) {
+		super();
+		this.nome = nome;
+		this.produto = produto;
+		this.desconto = desconto;
+		this.valorFrete = valorFrete;
+	
+	}
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
