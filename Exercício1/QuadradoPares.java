37a54c0ce845edb10a8ec0653d89df67370aaa3e
Rebeca <becalbuquerque_23@hotmail.com>
2023-10-25 16:47:23 -0300

    Calculando quadrado de pares

---
+++ Exercício1/QuadradoPares.java
@@ -0,0 +1,117 @@
+package Exercício1;
+
+import java.util.Scanner;
+
+public class QuadradoPares { 
+
+public static void main(String[] args) {
+    
+ 
+		Scanner entrada = new Scanner(System.in);
+		
+		int a = entrada.nextInt();
+		
+		
+		
+		
+		
+		for(int i = 2; i <= a; i += 2) {
+			int par = i * i;
+			
+			System.out.printf("%d^2 = %d\n", i, + par);
+		}
+		
+		
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
