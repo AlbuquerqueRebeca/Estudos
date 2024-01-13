d318bc7641fc9d52253fb5485dea18ddcba1da6a
Rebeca <becalbuquerque_23@hotmail.com>
2024-01-13 17:21:54 -0300

    criando um scanner

---
+++ CalculoTriangulo/Triangulo.java
@@ -0,0 +1,56 @@
+package CalculoTriangulo;
+
+import java.util.Scanner;
+
+public class Triangulo {
+
+
+public static void main(String[] args) {
+    
+ Scanner entrada = new Scanner(System.in);
+
+ double A = entrada.nextDouble();
+ double B = entrada.nextDouble();
+ double C = entrada.nextDouble();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
