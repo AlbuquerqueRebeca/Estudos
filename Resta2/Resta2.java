8f0c9a26cc996b1c32fdd67898dd112a902edbbf
Rebeca <becalbuquerque_23@hotmail.com>
2024-02-29 17:54:09 -0300

    Resta 2 divididos por N

---
+++ Resta2/Resta2.java
@@ -0,0 +1,62 @@
+package Resta2;
+
+import java.util.Scanner;
+
+public class Resta2{
+
+public static void main(String[] args) {
+    
+
+//lendo um valor inteiro
+
+Scanner entrada = new Scanner(System.in);
+
+
+int N = entrada.nextInt();
+
+
+for(int i = 1; i <= 10000; i ++){
+    if(i % N == 2){
+        System.out.println(i);
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
