b41b2f5fd05bfe6842b9ea5f5bf5395c9fe54011
Rebeca <becalbuquerque_23@hotmail.com>
2024-03-01 16:47:17 -0300

    taboada

---
+++ Taboada/Taboada.java
@@ -0,0 +1,77 @@
+package Taboada;
+
+import java.util.Scanner;
+
+public class Taboada{
+
+public static void main(String[] args) {
+    
+Scanner entrada = new Scanner(System.in);
+
+
+int N = entrada.nextInt(); //entrada de um numero inteiro
+
+
+int multiplicador = 1;
+while (multiplicador <= 10) {
+    
+    int resultado = N * multiplicador;
+    System.out.println( multiplicador + " x " + N + " = " + resultado);
+      multiplicador ++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
