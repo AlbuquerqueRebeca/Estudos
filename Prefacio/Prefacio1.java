010237e37ff3257e89553341e3e1334d9b29d977
Rebeca <becalbuquerque_23@hotmail.com>
2024-03-29 12:19:34 -0300

    calculando o quociente e o resto da divisão de dois numeros inteiros

---
+++ Prefacio/Prefacio1.java
@@ -0,0 +1,62 @@
+package Prefacio;
+
+import java.util.Scanner;
+
+public class Prefacio1{
+
+public static void main(String[] args) {
+    
+
+Scanner entrada = new Scanner(System.in);
+
+int a = entrada.nextInt();
+int b = entrada.nextInt();
+
+  if(b <= 0){  
+
+  } else{
+    int q = a / b;
+ int r = a % b; 
+ 
+ System.out.print(q);
+ System.out.print(" ");
+  System.out.print(r);
+
+
+
+
+
+  }
+
+
+ 
+ 
+
+
+ 
+
+
+
+ 
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
