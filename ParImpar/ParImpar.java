c8d7b619134e5d062d024765a2c40300b7f40d0e
Rebeca <becalbuquerque_23@hotmail.com>
2024-03-30 18:18:28 -0300

    verificando se o numero é impar ou par

---
+++ ParImpar/ParImpar.java
@@ -0,0 +1,105 @@
+package ParImpar;
+
+import java.util.Scanner;
+
+public class ParImpar{
+
+public static void main(String[] args) {
+    
+  Scanner entrada = new Scanner(System.in);
+
+
+    System.out.println("Digite o número: ");
+     int numero = entrada.nextInt();
+
+     if(numero % 2 == 0){
+        System.out.println("O número é PAR");
+     
+    } else {
+        System.out.println(" O numero é IMPAR");
+    }
+     
+      
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  entrada.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
