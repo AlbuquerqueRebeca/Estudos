cdb18a95f9e658d4d2858ccf7bb2f17cd41ccaa1
Rebeca <becalbuquerque_23@hotmail.com>
2024-04-20 17:32:14 -0300

    numero par/impar

---
+++ IfImpar/Impar.java
@@ -0,0 +1,88 @@
+package IfImpar;
+
+import java.util.Scanner;
+
+public class Impar{
+
+
+    public static void main(String[] args) {
+        
+
+     Scanner entrada = new Scanner(System.in);
+
+       System.out.println("Digite um numero: ");
+       int numero = entrada.nextInt();
+        
+
+       if(numero % 2 == 0){
+        System.out.println("Numero PAR");
+       }
+
+       if(numero % 2 == 1){
+       System.out.println("Numero Impar");
+
+       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
