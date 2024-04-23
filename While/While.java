496114148c9bce40907e3446e3296c7afb66f9af
Rebeca <becalbuquerque_23@hotmail.com>
2024-04-23 17:29:38 -0300

    While basico

---
+++ While/While.java
@@ -0,0 +1,67 @@
+package While;
+
+public class While{
+
+public static void main(String[] args) {
+    
+int base = 1;
+
+while(base <= 10){
+    System.out.println("Rebeca");
+    base ++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
