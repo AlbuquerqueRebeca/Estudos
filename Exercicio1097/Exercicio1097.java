59cd624f75bdef2d1f9f175e36e4d8e405355bd6
Rebeca <becalbuquerque_23@hotmail.com>
2024-04-27 18:45:31 -0300

    sequencia de numeros

---
+++ Exercicio1097/Exercicio1097.java
@@ -0,0 +1,73 @@
+package Exercicio1097;
+
+public class Exercicio1097{
+
+//10 11 12 
+public static void main(String[] args) {
+    
+System.out.println("I=1 J=7");
+System.out.println("I=1 J=6");
+System.out.println("I=1 J=5");
+System.out.println("I=3 J=9");
+System.out.println("I=3 J=8");         //5 6 7  8 9   10 11 12  13 14 
+System.out.println("I=3 J=7");
+System.out.println("I=5 J=11");
+System.out.println("I=5 J=10");
+System.out.println("I=5 J=9");
+System.out.println("I=7 J=13"); //13 //12
+System.out.println("I=7 J=12");
+System.out.println("I=7 J=11");
+System.out.println("I=9 J=15");
+System.out.println("I=9 J=14");
+System.out.println("I=9 J=13");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
