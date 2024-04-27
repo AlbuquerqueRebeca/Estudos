84ae31dea5561c8f06e0acb8471cb23e00ac56ce
Rebeca <becalbuquerque_23@hotmail.com>
2024-04-27 17:25:12 -0300

    sequencia de numeros

---
+++ Exercicio1095/Exercicio1095.java
@@ -0,0 +1,54 @@
+package Exercicio1095;
+
+
+public class Exercicio1095{
+
+public static void main(String[] args) {
+    
+
+  System.out.println("I=1 J=60");
+  System.out.println("I=4 J=55");
+  System.out.println("I=7 j=50");
+  System.out.println("I=10 J=45");
+  System.out.println("I=13 J=40");
+  System.out.println("I=16 J=35");
+  System.out.println("I=19 J=30");
+  System.out.println("I=22 J=25");
+  System.out.println("I=25 J=20");
+  System.out.println("I=28 J=15");
+  System.out.println("I=31 J=10");
+  System.out.println("I=34 J=5");
+  System.out.println("I=37 J=0");
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
