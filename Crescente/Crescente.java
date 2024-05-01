18aeb475ffd7afe43fa8f632c28b1cc27d0f70ae
Rebeca <becalbuquerque_23@hotmail.com>
2024-05-01 11:54:52 -0300

    crescente e decrescente

---
+++ Crescente/Crescente.java
@@ -0,0 +1,78 @@
+package Crescente;
+
+import java.util.Scanner;
+
+public class Crescente{
+
+    //2 entradas de numeros inteiros x e y 
+    //verificar se os numeros digitados estao em ordem crescente ou decrescente
+public static void main(String[] args) {
+    
+Scanner entrada = new Scanner(System.in);
+
+int x = entrada.nextInt();
+int y = entrada.nextInt();
+
+
+if(x > y ){
+  System.out.println("Decrescente");  
+}
+
+if(x < y){
+  System.out.println("Crescente");  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
