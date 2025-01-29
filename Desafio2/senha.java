759d6d4e8849e7552a481365791eaca12aeeb007
Rebeca <becalbuquerque_23@hotmail.com>
2025-01-29 18:16:29 -0300

    Iniciando o scanner

---
+++ Desafio2/senha.java
@@ -0,0 +1,43 @@
+package Desafio2;
+
+import java.util.Scanner;
+
+public class senha{
+
+     public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in); 
+
+        
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
