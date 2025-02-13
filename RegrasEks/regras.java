6573941ea276d43fce280c5797bcbad7c7ca8336
Rebeca <becalbuquerque_23@hotmail.com>
2025-02-13 18:46:49 -0300

    Inicializando o scanner

--- RegrasEks/regras.java
+++ RegrasEks/regras.java
@@ -1,11 +1,16 @@
 package RegrasEks;
 
+import java.util.Scanner;
+
 public class regras {
 
     public static void main(String[] args) {
 
         int podsPorNode = 10; 
         int podsPorServidor = 4; 
+
+
+        Scanner scanner = new Scanner(System.in); 
         
     }
     
