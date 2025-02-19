0c606ba0d43e432cc4777163d1475ee445053915
Rebeca <becalbuquerque_23@hotmail.com>
2025-02-19 18:38:10 -0300

    Iniciando o scanner

---
+++ VelocidadeDowload/CalculoVelocidade.java
@@ -0,0 +1,11 @@
+package VelocidadeDowload;
+
+import java.util.Scanner;
+
+public class CalculoVelocidade {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+   }
+    
+}
