df2f6018982be71bb26625129bc72a25fc780014
Rebeca <becalbuquerque_23@hotmail.com>
2025-02-25 19:23:56 -0300

    Adicionando a mensagem 4 valores positivos

--- ValoresNegativos/negativos.java
+++ ValoresNegativos/negativos.java
@@ -7,8 +7,23 @@ public class negativos {
         int numero1 = 7;
         int numero2 = 6; 
        double numero3 = 4.6; 
+       int numero4 = 12;
 
        int numero5 = 5;
+       double numero6 = 3.4;
+
+
+       System.out.println(numero1);
+       System.out.println(numero2);
+       System.out.println(numero3);
+       System.out.println(numero4);
+       System.out.println(" 4 valores positivos ");  
+       
+
+
+
+
+
     }
     
 }
