bcdaac2c3050520d2cfdd28fcc11d517c891799e
Rebeca <becalbuquerque_23@hotmail.com>
2025-03-22 19:02:57 -0300

    Inicializando o scanner

---
+++ Inalterado/LeituraInalterado.java
@@ -0,0 +1,11 @@
+package Inalterado;
+
+import java.util.Scanner;
+
+public class LeituraInalterado {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+    }
+    
+}
