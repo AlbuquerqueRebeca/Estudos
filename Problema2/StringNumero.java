77afbd47cef9e6a7768c71f0e72ceaf5b96804fa
Rebeca <becalbuquerque_23@hotmail.com>
2025-03-28 18:38:57 -0300

    Inicializando o scanner

--- Problema2/StringNumero.java
+++ Problema2/StringNumero.java
@@ -1,8 +1,12 @@
 package Problema2;
 
+import java.util.Scanner;
+
 public class StringNumero {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in); 
         
     }
     
