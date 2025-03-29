c6bc6906cd3c3e23d0325b9d41aceca4e94e6781
Rebeca <becalbuquerque_23@hotmail.com>
2025-03-29 18:49:28 -0300

    iniciando o metodo scanner

--- Potencia/potenciaN.java
+++ Potencia/potenciaN.java
@@ -1,10 +1,12 @@
 package Potencia;
 
+import java.util.Scanner;
+
 public enum potenciaN{
 
       public static void main(String[] args){
 
-     
+     Scanner scanner = new Scanner(System.in);
 
 
 
