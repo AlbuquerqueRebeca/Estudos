263ee55cb662a32afa8d425e8312490d626bd3ef
Rebeca <becalbuquerque_23@hotmail.com>
2025-04-02 18:06:25 -0300

    Inicializando o metodo scanner

---
+++ Problema4/Letras.java
@@ -0,0 +1,22 @@
+package Problema4;
+
+public class Letras{
+
+        public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in); 
+  
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+}
