19da769642d750fa9686dd3e613c91160d2f6a84
Rebeca <becalbuquerque_23@hotmail.com>
2025-06-26 18:43:54 -0300

    se i for só divisivel por 5

--- MultiplosTestes/Testes.java
+++ MultiplosTestes/Testes.java
@@ -13,8 +13,9 @@ for(int i = 0; i <= n; i ++){
         System.out.println("FizzBuzz");
      }else if(i % 3 == 0){ 
         System.out.println("Fizz");
+    } else if( i % 5 == 0){ 
 
-     }
+    }
 
 } 
 
